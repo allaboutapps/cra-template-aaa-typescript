f1eb4028c37dd12bc33f67108293f7d728794c15
Matthias Wild <matthias.wild@allaboutapps.at>
2020-01-13 09:12:53 +0100

    Recreate NotFoundSite as functional component

--- template/src/components/sites/NotFoundSite.tsx
+++ template/src/components/sites/NotFoundSite.tsx
@@ -1,14 +1,8 @@
 import * as React from "react";
 import { t } from "../../i18n/util";
 
-class NotFoundSite extends React.Component {
-    render() {
-        return (
-            <h1 style={{ margin: 24, textAlign: "center" }}>
-                {t("screen.not_found.title")}
-            </h1>
-        );
-    }
-}
-
-export { NotFoundSite };
+export const NotFoundSite = () => (
+    <h1 style={{ margin: 24, textAlign: "center" }}>
+        {t("screen.not_found.title")}
+    </h1>
+)
