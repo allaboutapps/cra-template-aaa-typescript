37ed893e2436c8166ccc8fbab1eee10d37407d0a
Matthias Wild <matthias.wild@allaboutapps.at>
2020-01-13 09:30:11 +0100

    remove unecessary function body in LoadingOverlay

--- template/src/components/ui/LoadingOverlay.tsx
+++ template/src/components/ui/LoadingOverlay.tsx
@@ -5,24 +5,23 @@ interface IProps {
     isVisible: boolean;
 }
 
-export const LoadingOverlay = ({ isVisible }: IProps) => {
-    return (
-        <div
-            style={{
-                display: isVisible ? "flex" : "none",
-                position: "fixed",
-                width: "100%",
-                height: "100%",
-                background: "rgba(0, 0, 0, 0.75)",
-                alignItems: "center",
-                justifyContent: "center",
-                left: 0,
-                top: 0,
-                zIndex: 9999
-            }}
-        >
-            <CircularProgress color="secondary" />
-        </div>
-    );
-}
+export const LoadingOverlay = ({ isVisible }: IProps) => (
+    <div
+        style={{
+            display: isVisible ? "flex" : "none",
+            position: "fixed",
+            width: "100%",
+            height: "100%",
+            background: "rgba(0, 0, 0, 0.75)",
+            alignItems: "center",
+            justifyContent: "center",
+            left: 0,
+            top: 0,
+            zIndex: 9999
+        }}
+    >
+        <CircularProgress color="secondary" />
+    </div>
+)
+
 
