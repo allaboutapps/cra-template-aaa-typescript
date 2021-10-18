dc26743d5a48d1c2ae8d6e49e04eea4983f52a34
Matthias Wild <matthias.wild@allaboutapps.at>
2021-10-18 09:02:55 +0200

    use styled for FieldError

--- template/src/components/ui/FieldError.tsx
+++ template/src/components/ui/FieldError.tsx
@@ -1,21 +1,12 @@
+import { styled } from "@mui/system";
 import * as React from "react";
 
-type FieldErrorProps = {
-    children?: string;
-};
-
-export const FieldError = ({ children }: FieldErrorProps) => (
-    <span
-        style={{
-            color: "#d32f2f",
-            display: "block",
-            minHeight: 18,
-            width: "100%",
-            marginTop: 4,
-            marginBottom: 10,
-            fontSize: 12,
-        }}
-    >
-        {children}
-    </span>
-);
+export const FieldError = styled("div")({
+    color: "#d32f2f",
+    display: "block",
+    minHeight: 18,
+    width: "100%",
+    marginTop: 4,
+    marginBottom: 10,
+    fontSize: 12,
+});
