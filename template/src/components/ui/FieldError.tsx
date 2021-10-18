76d69aaf12f61c1abc4175047928453ef507e383
Matthias Wild <matthias.wild@allaboutapps.at>
2021-10-18 12:38:32 +0200

    use span as before for FieldError

--- template/src/components/ui/FieldError.tsx
+++ template/src/components/ui/FieldError.tsx
@@ -1,7 +1,7 @@
 import { styled } from "@mui/system";
 import * as React from "react";
 
-export const FieldError = styled("div")({
+export const FieldError = styled("span")({
     color: "#d32f2f",
     display: "block",
     minHeight: 18,
