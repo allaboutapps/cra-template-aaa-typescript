70425d753fbe2b49504514f4362f9aa21179bef7
jlangela <jan.langela.regincos@allaboutapps.at>
2022-05-16 14:11:48 +0200

    added SwitchProps

--- template/src/components/app/router/CustomSwitch.tsx
+++ template/src/components/app/router/CustomSwitch.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-import { Route, Switch } from "react-router";
+import { Route, Switch, SwitchProps } from "react-router-dom";
 import { NotFoundSite } from "../../shared/sites/NotFoundSite";
 
-export const CustomSwitch = ({ children, ...props }: React.ComponentProps<typeof Switch>) => (
+export const CustomSwitch = ({ children, ...props }: SwitchProps) => (
     <Switch {...props}>
         {children}
         <Route>
