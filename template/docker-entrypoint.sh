0fe081e4df17cd7cb8ab937c5300785b533e4d71
Christoph Werner <christoph@chrztoph.com>
2022-10-19 17:17:05 +0200

    Fix handling of base names which are /

--- template/docker-entrypoint.sh
+++ template/docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+# This file is run inside the nginx container, which serves the frontend, as ENTRYPOINT.
+# It is responsible for replacing dynamic configuration at runtime in various files.
+
 set -e
 
-# Only update config with defined env variables if we"re running an nginx process so we can still attach with debugging commands
+# Only update config with defined env variables if we are running an nginx process so we can still attach with debugging commands
 # without accidentally modifying anything
 if [ "$1" == "nginx" ]; then
     if [ ! -z "${REACT_APP_API_BASE_URL}" ]; then
@@ -12,7 +15,12 @@ if [ "$1" == "nginx" ]; then
     if [ ! -z "${REACT_APP_BASE_NAME}" ]; then
         sed -i "/ENV_BASE_NAME/c\var ENV_BASE_NAME = \"${REACT_APP_BASE_NAME}\";" /etc/nginx/html/config.js
         sed -i "s#REACT_APP_BASE_NAME#$REACT_APP_BASE_NAME#g" /etc/nginx/conf.d/default.conf | sed "s#//#/#g"
-        sed -i "s#<base href=\"/\">#<base href=\"$REACT_APP_BASE_NAME/\">#g" /etc/nginx/html/index.html | sed "s#<base href=\"//\">#<base href=\"/\">#g"
+
+        # The <base href="/"> is already set to "/". If the REACT_APP_BASE_NAME is a "/", it will change the base href to "//" which is invalid.
+        # Therefore we only run the command if the REACT_APP_BASE_NAME is not "/".
+        if [ "$REACT_APP_BASE_NAME" != "/" ]; then
+            sed -i "s#<base href=\"/\">#<base href=\"$REACT_APP_BASE_NAME/\">#g" /etc/nginx/html/index.html | sed "s#<base href=\"//\">#<base href=\"/\">#g"
+        fi
     fi
 fi
 
